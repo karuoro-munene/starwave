35b799d0c6637b1f138af9884f1a36eb64e6818b
Cyprian Munene <45110252+karuoro-munene@users.noreply.github.com>
2024-06-05 22:47:24 -0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,5 @@ python3.9 manage.py migrate
 python3.9 manage.py collectstatic
 
 #create admin
-DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@clinton.dev DJANGO_SUPERUSER_PASSWORD=droidstarwave python3.9 manage.py createsuperuser --noinput
+#DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@clinton.dev DJANGO_SUPERUSER_PASSWORD=droidstarwave python3.9 manage.py createsuperuser --noinput
 
