d84a264a84a4526ea7cc5dfd0dfe0ea065b5f0e2
Cyprian Munene <45110252+karuoro-munene@users.noreply.github.com>
2024-06-05 22:19:48 -0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,5 @@ python3.9 manage.py migrate
 python3.9 manage.py collectstatic
 
 #create admin
-python3.9 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@clinton', 'droid')"
+# python3.9 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@clinton', 'droid')"
 
