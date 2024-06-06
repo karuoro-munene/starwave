0a1b0086b678854967debcb7f062a68c2a003b0f
Cyprian Munene <45110252+karuoro-munene@users.noreply.github.com>
2024-06-05 22:26:02 -0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,5 @@ python3.9 manage.py migrate
 python3.9 manage.py collectstatic
 
 #create admin
-# python3.9 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@clinton', 'droid')"
+DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@clinton.dev DJANGO_SUPERUSER_PASSWORD=droidstarwave python3.9 manage.py createsuperuser --noinput
 
